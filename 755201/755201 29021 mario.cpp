2295bc0d9308483e406a58b3ef3a4a6f1bf42b7a
Ning1120 <71584065+Ning1120@users.noreply.github.com>
2020-10-08 19:15:29 +0800

    Update 755201 29021 mario.cpp

--- 755201/755201 29021 mario.cpp
+++ 755201/755201 29021 mario.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int hight;
-    cout << "°ª«×: ";
+    cout << "high: ";
     cin >> hight;
     for(int e = hight;e > 0;e--) {
         for(int i = e;i > 0;i--) {
